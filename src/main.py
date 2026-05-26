c2b78adebc96b5e4c198c95f53e548808aad2ad4
masatn1973 <masatn1973@gmail.com>
2026-05-26 11:53:29 +0900

    Fixed the code

--- src/main.py
+++ src/main.py
@@ -42,9 +42,6 @@ resource._register()
 
 from .window import ImageViewerWindow
 
-resource = Gio.Resource.load("/app/share/imageviewer/imageviewer.gresource")
-resource._register()
-
 
 class ImageviewerApplication(Adw.Application):
     """The main application singleton class."""
